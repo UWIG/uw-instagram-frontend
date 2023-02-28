951a0bb418a5bb0fe2ab45846f29c6d981a122e3
y2792li <y2792li@uwaterloo.ca>
2023-02-28 17:40:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+
+const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/sign-up'));
+
+function App() {
+  return (
+    // <button disabled="false" type="button" className='bg-slate-50'>
+    //         Button
+    // </button>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import FirebaseContext from './context/firebase';
+import { firebase, FieldValue } from './lib/firebase';
+import './styles/app.css';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <App />
+// );
+
+ReactDOM.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
+
+// client side rendered app: react (cra)
+// -> database wihch is Firebase
+// -> react-loading-skeleton
+// tailwind
+
+// architecture
+// src
+// -> components,
+// -> constants,
+// -> context,
+// -> helpers,
+// -> hooks,
+// -> pages,
+// -> lib (firebase is going to live in here),
+// -> services (firebase functions in here),
+// -> styles (tailwind's folder (app/tailwind))
